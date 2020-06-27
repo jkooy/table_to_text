bb85001d844f3aa97159b5190eb9eb955b80eb73
xuehai he <x5he@ucsd.com>
2020-06-26 19:16:44 -0700

    update

--- DataLoader.py
+++ DataLoader.py
@@ -6,6 +6,7 @@
 import tensorflow as tf
 import time
 import numpy as np
+import re, time, os
 
 
 class DataLoader(object):
@@ -24,10 +25,10 @@ class DataLoader(object):
         start_time = time.time()
 
         print('Reading datasets ...')
-        self.train_set = self.load_data(self.train_data_path)
+#         self.train_set = self.load_data(self.train_data_path)
         self.test_set = self.load_data(self.test_data_path)
         # self.small_test_set = self.load_data(self.small_test_data_path)
-        self.dev_set = self.load_data(self.dev_data_path)
+#         self.dev_set = self.load_data(self.dev_data_path)
         print ('Reading datasets comsumes %.3f seconds' % (time.time() - start_time))
 
     def load_data(self, path):
@@ -45,14 +46,148 @@ class DataLoader(object):
             rposes = rposes[:self.limits]
         print summaries[0].strip().split(' ')
         summaries = [list(map(int, summary.strip().split(' '))) for summary in summaries]
+        
+        print('................texts.................', texts[0])
         texts = [list(map(int, text.strip().split(' '))) for text in texts]
         fields = [list(map(int, field.strip().split(' '))) for field in fields]
         poses = [list(map(int, pos.strip().split(' '))) for pos in poses]
         rposes = [list(map(int, rpos.strip().split(' '))) for rpos in rposes]
         return summaries, texts, fields, poses, rposes
+    
+    def single_test(self):
+        class Vocab(object):
+            def __init__(self):
+                vocab = dict()
+                vocab['PAD'] = 0
+                vocab['START_TOKEN'] = 1
+                vocab['END_TOKEN'] = 2
+                vocab['UNK_TOKEN'] = 3
+                cnt = 4
+                with open("original_data/word_vocab.txt", "r") as v:
+                    for line in v:
+                        word = line.strip().split()[0]
+                        vocab[word] = cnt
+                        cnt += 1
+                self._word2id = vocab
+                self._id2word = {value: key for key, value in vocab.items()}
 
+                key_map = dict()
+                key_map['PAD'] = 0
+                key_map['START_TOKEN'] = 1
+                key_map['END_TOKEN'] = 2
+                key_map['UNK_TOKEN'] = 3
+                cnt = 4
+                with open("original_data/field_vocab.txt", "r") as v:
+                    for line in v:
+                        key = line.strip().split()[0]
+                        key_map[key] = cnt
+                        cnt += 1
+                self._key2id = key_map
+                self._id2key = {value: key for key, value in key_map.items()}
+
+            def word2id(self, word):
+                ans = self._word2id[word] if word in self._word2id else 3
+                return ans
+
+            def id2word(self, id):
+                ans = self._id2word[int(id)]
+                return ans
+
+            def key2id(self, key):
+                ans = self._key2id[key] if key in self._key2id else 3
+                return ans
+
+            def id2key(self, id):
+                ans = self._id2key[int(id)]
+                return ans
+
+
+        fboxes = "original_data/test.box"
+
+        mixb_word, mixb_label, mixb_pos = [], [], []
+
+        box = open(fboxes, "r").read().strip().split('\n')
+        box_word, box_label, box_pos = [], [], []
+
+        print(box[0])
+        ib = box[0]
+
+        item = ib.split('\t')
+        box_single_word, box_single_label, box_single_pos = [], [], []
+        for it in item:
+            if len(it.split(':')) > 2:
+                continue
+            # print it
+            prefix, word = it.split(':')
+            if '<none>' in word or word.strip()=='' or prefix.strip()=='':
+                continue
+            new_label = re.sub("_[1-9]\d*$", "", prefix)
+            if new_label.strip() == "":
+                continue
+            box_single_word.append(word)
+            box_single_label.append(new_label)
+            if re.search("_[1-9]\d*$", prefix):
+                field_id = int(prefix.split('_')[-1])
+                box_single_pos.append(field_id if field_id<=30 else 30)
+            else:
+                box_single_pos.append(1)
+        box_word.append(box_single_word)
+        box_label.append(box_single_label)
+        box_pos.append(box_single_pos)
+
+
+        ######################## reverse box #############################
+        box = box_pos
+        tmp_pos = []
+        single_pos = []
+        reverse_pos = []
+        for pos in box:
+            tmp_pos = []
+            single_pos = []
+            for p in pos:
+                if int(p) == 1 and len(tmp_pos) != 0:
+                    single_pos.extend(tmp_pos[::-1])
+                    tmp_pos = []
+                tmp_pos.append(p)
+            single_pos.extend(tmp_pos[::-1])
+            reverse_pos = single_pos
+
+    
+    
+        vocab = Vocab()
+
+        texts = (" ".join([str(vocab.word2id(word)) for word in box_word[0]]) + '\n')
+        text = list(map(int,texts.strip().split(' ')))
+        print(text)
+
+        fields = (" ".join([str(vocab.key2id(word)) for word in box_label[0]]) + '\n')
+        field = list(map(int,fields.strip().split(' ')))
+        print(field)
+
+        pos = box_pos[0]
+        print(pos)   
+
+        rpos = reverse_pos
+        print(rpos)
+               
+        text_len = len(text)
+        pos_len = len(pos)
+        rpos_len = len(rpos)
+                
+        batch_data = {'enc_in':[], 'enc_fd':[], 'enc_pos':[], 'enc_rpos':[], 'enc_len':[],
+                          'dec_in':[], 'dec_len':[], 'dec_out':[]}
+        
+        batch_data['enc_in'].append(text)
+        batch_data['enc_len'].append(text_len)
+        batch_data['enc_fd'].append(field)
+        batch_data['enc_pos'].append(pos)
+        batch_data['enc_rpos'].append(rpos)
+        
+        yield batch_data
+        
     def batch_iter(self, data, batch_size, shuffle):
         summaries, texts, fields, poses, rposes = data
+        
         data_size = len(summaries)
         num_batches = int(data_size / batch_size) if data_size % batch_size == 0 \
             else int(data_size / batch_size) + 1
@@ -105,5 +240,4 @@ class DataLoader(object):
                 batch_data['dec_in'].append(summary)
                 batch_data['dec_len'].append(summary_len)
                 batch_data['dec_out'].append(gold)
-  
             yield batch_data
--- Main.py
+++ Main.py
@@ -26,7 +26,7 @@ tf.app.flags.DEFINE_integer("hidden_size", 500, "Size of each layer.")
 tf.app.flags.DEFINE_integer("emb_size", 400, "Size of embedding.")
 tf.app.flags.DEFINE_integer("field_size", 50, "Size of embedding.")
 tf.app.flags.DEFINE_integer("pos_size", 5, "Size of embedding.")
-tf.app.flags.DEFINE_integer("batch_size", 32, "Batch size of train set.")
+tf.app.flags.DEFINE_integer("batch_size", 1, "Batch size of train set.")
 tf.app.flags.DEFINE_integer("epoch", 50, "Number of training epoch.")
 tf.app.flags.DEFINE_integer("source_vocab", 20003,'vocabulary size')
 tf.app.flags.DEFINE_integer("field_vocab", 1480,'vocabulary size')
@@ -111,7 +111,8 @@ def train(sess, dataloader, model):
 
 def test(sess, dataloader, model):
     logging.info("test = %s", model)
-    evaluate(sess, dataloader, model, save_dir, 'test')
+#     evaluate(sess, dataloader, model, save_dir, 'test')
+    output(sess, dataloader, model, save_dir, 'test')
 
 def save_model(model, save_dir, cnt):
     new_dir = save_dir + 'loads' + '/' 
@@ -122,6 +123,235 @@ def save_model(model, save_dir, cnt):
         os.mkdir(nnew_dir)
     model.save(nnew_dir)
     return nnew_dir
+
+
+class Vocab(object):
+    def __init__(self):
+        vocab = dict()
+        vocab['PAD'] = 0
+        vocab['START_TOKEN'] = 1
+        vocab['END_TOKEN'] = 2
+        vocab['UNK_TOKEN'] = 3
+        cnt = 4
+        with open("original_data/word_vocab.txt", "r") as v:
+            for line in v:
+                word = line.strip().split()[0]
+                vocab[word] = cnt
+                cnt += 1
+        self._word2id = vocab
+        self._id2word = {value: key for key, value in vocab.items()}
+
+        key_map = dict()
+        key_map['PAD'] = 0
+        key_map['START_TOKEN'] = 1
+        key_map['END_TOKEN'] = 2
+        key_map['UNK_TOKEN'] = 3
+        cnt = 4
+        with open("original_data/field_vocab.txt", "r") as v:
+            for line in v:
+                key = line.strip().split()[0]
+                key_map[key] = cnt
+                cnt += 1
+        self._key2id = key_map
+        self._id2key = {value: key for key, value in key_map.items()}
+
+    def word2id(self, word):
+        ans = self._word2id[word] if word in self._word2id else 3
+        return ans
+
+    def id2word(self, id):
+        ans = self._id2word[int(id)]
+        return ans
+
+    def key2id(self, key):
+        ans = self._key2id[key] if key in self._key2id else 3
+        return ans
+
+    def id2key(self, id):
+        ans = self._id2key[int(id)]
+        return ans
+    
+def output(sess, dataloader, model, ksave_dir, mode='valid'):
+    if mode == 'valid':
+        # texts_path = "original_data/valid.summary"
+        texts_path = "processed_data/valid/valid.box.val"
+        gold_path = gold_path_valid
+        evalset = dataloader.dev_set
+    else:
+        # texts_path = "original_data/test.summary"
+        texts_path = "processed_data/test/test.box.val"
+        gold_path = gold_path_test
+        evalset = dataloader.test_set
+    
+    # for copy words from the infoboxes
+    v = Vocab()
+
+    # with copy
+    pred_list, pred_list_copy, gold_list = [], [], []
+    pred_unk, pred_mask = [], []
+    
+    print('..............Begin test iteration...........')
+    print('Total bacth number is:', len(list(dataloader.batch_iter(evalset, FLAGS.batch_size, False))))
+    k = 0
+    
+    fboxes = "original_data/test.box"
+
+    mixb_word, mixb_label, mixb_pos = [], [], []
+
+    box_word, box_label, box_pos = [], [], []
+#     ib = box[0]
+
+
+    ib = 'name_1:xuehau he\tname_2:ssss\tposition_1:second\tposition_2:hhhh'
+    item = ib.split('\t')
+    box_single_word, box_single_label, box_single_pos = [], [], []
+    for it in item:
+        if len(it.split(':')) > 2:
+            continue
+        # print it
+        prefix, word = it.split(':')
+        if '<none>' in word or word.strip()=='' or prefix.strip()=='':
+            continue
+        new_label = re.sub("_[1-9]\d*$", "", prefix)
+        if new_label.strip() == "":
+            continue
+        box_single_word.append(word)
+        box_single_label.append(new_label)
+        if re.search("_[1-9]\d*$", prefix):
+            field_id = int(prefix.split('_')[-1])
+            box_single_pos.append(field_id if field_id<=30 else 30)
+        else:
+            box_single_pos.append(1)
+    box_word.append(box_single_word)
+    box_label.append(box_single_label)
+    box_pos.append(box_single_pos)
+
+
+    ######################## reverse box #############################
+    box = box_pos
+    tmp_pos = []
+    single_pos = []
+    reverse_pos = []
+    for pos in box:
+        tmp_pos = []
+        single_pos = []
+        for p in pos:
+            if int(p) == 1 and len(tmp_pos) != 0:
+                single_pos.extend(tmp_pos[::-1])
+                tmp_pos = []
+            tmp_pos.append(p)
+        single_pos.extend(tmp_pos[::-1])
+        reverse_pos = single_pos
+
+
+    vocab = Vocab()
+
+    textss = (" ".join([str(vocab.word2id(word)) for word in box_word[0]]) + '\n')
+    text = list(map(int,textss.strip().split(' ')))
+
+    fields = (" ".join([str(vocab.key2id(word)) for word in box_label[0]]) + '\n')
+    field = list(map(int,fields.strip().split(' ')))
+
+    pos = box_pos[0]
+
+    rpos = reverse_pos
+
+    text_len = len(text)
+    pos_len = len(pos)
+    rpos_len = len(rpos)
+
+    batch_data = {'enc_in':[], 'enc_fd':[], 'enc_pos':[], 'enc_rpos':[], 'enc_len':[],
+                      'dec_in':[], 'dec_len':[], 'dec_out':[]}
+
+    batch_data['enc_in'].append(text)
+    batch_data['enc_len'].append(text_len)
+    batch_data['enc_fd'].append(field)
+    batch_data['enc_pos'].append(pos)
+    batch_data['enc_rpos'].append(rpos)
+        
+    print('.......................input data........................', batch_data)
+    predictions, atts = model.generate(batch_data, sess)
+    atts = np.squeeze(atts)
+    idx = 0
+    print('path is:', pred_path + str(k))
+#         print('x is:',x)
+    for summary in np.array(predictions):
+        with open(pred_path + str(k), 'w') as sw:
+            summary = list(summary)
+            if 2 in summary:
+                summary = summary[:summary.index(2)] if summary[0] != 2 else [2]
+            real_sum, unk_sum, mask_sum = [], [], []
+            for tk, tid in enumerate(summary):
+#                 if tid == 3:
+#                     print('npmax',np.argmax(atts[tk,: len(texts[k])]))
+#                     print('mm',np.argmax(atts[tk,: len(texts[k])]))
+#                     print('texts',np.shape(texts))
+#                     print('texts_full',texts[k])
+# #                         sub = texts[k]
+#                     real_sum=texts[k]
+# #                 else:
+# #                     real_sum.append(v.id2word(tid))
+                real_sum.append(v.id2word(tid))
+    print('pred set is:', ' '.join(real_sum))
+    return pred_list  
+    
+    '''
+    for x in dataloader.single_test():
+        print('.......................single test........................',x)
+#         def generate(self, x, sess):
+#         predictions, atts = sess.run([self.g_tokens, self.atts],
+#                                {self.encoder_input: x['enc_in'], self.encoder_field: x['enc_fd'], 
+#                                 self.encoder_len: x['enc_len'], self.encoder_pos: x['enc_pos'],
+#                                 self.encoder_rpos: x['enc_rpos']})
+#         return predictions, atts
+        predictions, atts = model.generate(x, sess)
+        atts = np.squeeze(atts)
+        idx = 0
+        print('path is:', pred_path + str(k))
+#         print('x is:',x)
+        for summary in np.array(predictions):
+            with open(pred_path + str(k), 'w') as sw:
+                summary = list(summary)
+                if 2 in summary:
+                    summary = summary[:summary.index(2)] if summary[0] != 2 else [2]
+                real_sum, unk_sum, mask_sum = [], [], []
+                for tk, tid in enumerate(summary):
+                    if tid == 3:
+                        print('npmax',np.argmax(atts[tk,: len(texts[k])]))
+                        print('mm',np.argmax(atts[tk,: len(texts[k])]))
+                        print('texts',np.shape(texts))
+                        print('texts_full',texts[k])
+#                         sub = texts[k]
+#                         real_sum=texts[k]
+#                         mask_sum.append("**" + str(sub) + "**")
+                    else:
+                        real_sum.append(v.id2word(tid))
+#                         mask_sum.append(v.id2word(tid))
+#                     unk_sum.append(v.id2word(tid))
+#                 sw.write(" ".join([str(x) for x in real_sum]) + '\n')
+#                 pred_list.append([str(x) for x in real_sum])
+#                 k += 1
+#                 idx += 1
+        break
+#     write_word(pred_mask, ksave_dir, mode + "_summary_copy.txt")
+#     write_word(pred_unk, ksave_dir, mode + "_summary_unk.txt")
+
+
+#     for tk in range(k):
+#         with open(gold_path + str(tk), 'r') as g:
+#             gold_list.append([g.read().strip()])
+
+#     gold_set = [[gold_path + str(i)] for i in range(k)]
+#     pred_set = [pred_path + str(i) for i in range(k)]
+    
+#     print('gold set is:', ','.join(str(gold_list[0])))
+    print(real_sum)
+    print('pred set is:', ' '.join(real_sum))
+#     print('pred set is:', [i for p in real_sum for i in p])
+    
+    return pred_list
+    '''
+    
 
 def evaluate(sess, dataloader, model, ksave_dir, mode='valid'):
     if mode == 'valid':
@@ -137,7 +367,9 @@ def evaluate(sess, dataloader, model, ksave_dir, mode='valid'):
     
     # for copy words from the infoboxes
     texts = open(texts_path, 'r').read().strip().split('\n')
+    print('test', texts[0])
     texts = [list(t.strip().split()) for t in texts]
+#     print('test split', texts)
     v = Vocab()
 
     # with copy
@@ -147,6 +379,36 @@ def evaluate(sess, dataloader, model, ksave_dir, mode='valid'):
     print('..............Begin test iteration...........')
     print('Total bacth number is:', len(list(dataloader.batch_iter(evalset, FLAGS.batch_size, False))))
     k = 0
+    
+    
+#     x = dataloader.batch_iter(evalset, FLAGS.batch_size, False)[0]
+#     predictions, atts = model.generate(x, sess)
+#     atts = np.squeeze(atts)
+#     idx = 0
+#     print('path is:', pred_path + str(k))
+#         print('x is:',x)
+#     for summary in np.array(predictions):
+#         with open(pred_path + str(k), 'w') as sw:
+#             summary = list(summary)
+#             if 2 in summary:
+#                 summary = summary[:summary.index(2)] if summary[0] != 2 else [2]
+#             real_sum, unk_sum, mask_sum = [], [], []
+#             for tk, tid in enumerate(summary):
+#                 if tid == 3:
+#                     sub = texts[k][np.argmax(atts[tk,: len(texts[k]),idx])]
+#                     real_sum.append(sub)
+#                     mask_sum.append("**" + str(sub) + "**")
+#                 else:
+#                     real_sum.append(v.id2word(tid))
+#                     mask_sum.append(v.id2word(tid))
+#                 unk_sum.append(v.id2word(tid))
+#             sw.write(" ".join([str(x) for x in real_sum]) + '\n')
+#             pred_list.append([str(x) for x in real_sum])
+#             pred_unk.append([str(x) for x in unk_sum])
+#             pred_mask.append([str(x) for x in mask_sum])
+#             k += 1
+#             idx += 1
+
     for x in dataloader.batch_iter(evalset, FLAGS.batch_size, False):
         predictions, atts = model.generate(x, sess)
         atts = np.squeeze(atts)
@@ -174,6 +436,7 @@ def evaluate(sess, dataloader, model, ksave_dir, mode='valid'):
                 pred_mask.append([str(x) for x in mask_sum])
                 k += 1
                 idx += 1
+        break
     write_word(pred_mask, ksave_dir, mode + "_summary_copy.txt")
     write_word(pred_unk, ksave_dir, mode + "_summary_unk.txt")
 
@@ -185,8 +448,10 @@ def evaluate(sess, dataloader, model, ksave_dir, mode='valid'):
     gold_set = [[gold_path + str(i)] for i in range(k)]
     pred_set = [pred_path + str(i) for i in range(k)]
     
-    print('gold set is:', gold_set)
-    print('pred set is:', pred_set)
+    print('gold set is:', ','.join(str(gold_list[0])))
+    print('pred set is:', ','.join(str(pred_list[0])))
+#     print('pred set is:', pred_unk[0],'\n \n \n')
+#     print('pred set is:', pred_mask[0])
     recall, precision, F_measure = PythonROUGE(pred_set, gold_set, ngram_order=4)
     bleu = corpus_bleu(gold_list, pred_list)
     copy_result = "with copy F_measure: %s Recall: %s Precision: %s BLEU: %s\n" % \
@@ -223,6 +488,7 @@ def main():
     with tf.Session(config=config) as sess:
         copy_file(save_file_dir)
         dataloader = DataLoader(FLAGS.dir, FLAGS.limits)
+#         print('dataloader',dataloader.test_set[0])
         model = SeqUnit(batch_size=FLAGS.batch_size, hidden_size=FLAGS.hidden_size, emb_size=FLAGS.emb_size,
                         field_size=FLAGS.field_size, pos_size=FLAGS.pos_size, field_vocab=FLAGS.field_vocab,
                         source_vocab=FLAGS.source_vocab, position_vocab=FLAGS.position_vocab,
